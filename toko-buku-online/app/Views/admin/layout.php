ed737e35fa3106c7d02caa7c2746e5c7295bfdae
DzikriAryaSahputra <tomoriid62@gmail.com>
2024-12-08 11:40:39 +0700

    Toko Buku Online - DataBuku

--- toko-buku-online/app/Views/admin/layout.php
+++ toko-buku-online/app/Views/admin/layout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="utf-8">
@@ -11,13 +12,14 @@
     <title>Admin Dashboard</title>
 
     <!-- Fontawsome -->
-    <link href="<?=base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles -->
-    <link href="<?=base_url('assets/css/sb-admin-2.min.css');?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 
 </head>
+
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -82,28 +84,28 @@
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
         </ul>
-        
+
         <!-- render untuk content databuku -->
-      <?= $this->renderSection('content'); ?> 
-        
-       <!-- Bootstrap core JavaScript-->
-    <script src="<?=base_url('assets/vendor/jquery/jquery.min.js');?>"></script>
-    <script src="<?=base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+        <?= $this->renderSection('content'); ?>
 
-    <!-- Core plugin JavaScript-->
-    <script src="<?=base_url('assets/vendor/jquery-easing/jquery.easing.min.js');?>"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+        <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="<?=base_url('assets/js/sb-admin-2.min.js')?>"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
-    <!-- Page level plugins -->
-    <script src="<?=base_url('assets/vendor/chart.js/Chart.min.js');?>"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="<?=base_url('assets/js/demo/chart-area-demo.js');?>"></script>
-    <script src="<?=base_url('assets/js/demo/chart-pie-demo.js');?>"></script>
+        <!-- Page level plugins -->
+        <script src="<?= base_url('assets/vendor/chart.js/Chart.min.js'); ?>"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+        <!-- Page level custom scripts -->
+        <script src="<?= base_url('assets/js/demo/chart-area-demo.js'); ?>"></script>
+        <script src="<?= base_url('assets/js/demo/chart-pie-demo.js'); ?>"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
 </body>
 
 </html>
