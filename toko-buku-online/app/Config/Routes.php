4c73ff0f0f91acbb47dfc7aa212b7ee52c4c51c2
DzikriAryaSahputra <tomoriid62@gmail.com>
2024-12-19 11:14:39 +0700

    toko-buku function-CRUD

--- toko-buku-online/app/Config/Routes.php
+++ toko-buku-online/app/Config/Routes.php
@@ -12,6 +12,8 @@ $routes->get('chart', 'Home::chart');
 $routes->get('checkout', 'Home::checkout');
 $routes->get('search', 'Home::search');
 $routes->post('submit', 'Home::submit');
+$routes->get('images/(:segment)','Home::image/$1');
+
 service('auth')->routes($routes);
 
 
@@ -24,5 +26,9 @@ $routes->group('admin',['filter'=> 'group:admin'],function($routes){
   $routes->get('pelanggan','AdminController::pelanggan');
   $routes->get('dashboard','AdminController::dashboard');
   $routes->get('transaksi','AdminController::transaksi');
+  $routes->post('databuku', "AdminController::create_buku");
+  $routes->get('databuku/(:segment)/edit', 'AdminController::edit_buku/$1');
+  $routes->post('databuku/(:segment)/update', 'AdminController::update_buku/$1');
+
 
 });
